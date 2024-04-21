17a9cd566a626a03193462a28be2da23a66a2c51
DaniilPrikhoddko <d1912dd@gmail.com>
2024-04-21 22:58:38 +0300

    Начало 8й лабы

--- Calculator/CalculatorX/MainWindow.xaml.cs
+++ Calculator/CalculatorX/MainWindow.xaml.cs
@@ -15,6 +15,18 @@ namespace CalculatorX
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    static class PointExtensions
+    {
+        public static Point ToMathCoordinates(this Point point, Canvas canvas, float zoom)
+        {
+            return new Point((point.X - canvas.ActualWidth / 2) / zoom, (point.Y - canvas.ActualHeight / 2) / zoom);
+        }
+
+        public static Point ToUiCoordinates(this Point point, Canvas canvas, float zoom)
+        {
+            return new Point((point.X * zoom + canvas.ActualWidth / 2), canvas.ActualHeight / 2 - point.Y * zoom);
+        }
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,6 +37,14 @@ namespace CalculatorX
         {
             float valueOfVariable = float.Parse(tbVariableValue.Text);
             lblResult.Content = new RpnCalculator(tbExpression.Text).Calculate(valueOfVariable);
+        }
+        private void cForGraphic_MouseMove(object sender, MouseEventArgs e)
+        {
+            var uiPoint = Mouse.GetPosition(cForGraphic);
+            var zoom = float.Parse(tbZoom.Text);
+            var mathPoint = Mouse.GetPosition(cForGraphic).ToMathCoordinates(cForGraphic, zoom);
+            lblUiCoordinates.Content = $"{uiPoint.X:0.#},{uiPoint.Y:0.#}";
+            lblMathCoordinates.Content = $"{mathPoint.X:0.#},{mathPoint.Y:0.#}";
         }
     }
 }
